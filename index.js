64a965d99d022352f731123a02d0f65caf56dedb
bessyhuang <42068007+bessyhuang@users.noreply.github.com>
2020-01-24 15:21:05 +0800

    Finish: place-search-bot (modify)

--- index.js
+++ index.js
@@ -63,49 +63,6 @@ function handleEvent(event) {
 }
 */
 
-/*
-// 新增 img-uploader-bot
-function handleEvent(event) {
-  if (event.replyToken === "00000000000000000000000000000000" || event.replyToken === "ffffffffffffffffffffffffffffffff") {
-    return Promise.resolve(null);
-  }
-  if (event.type === 'message' && event.message.type === 'image') {
-    if (event.message.contentProvider.type === "line") {
-      client.getMessageContent(event.message.id)
-        .then((stream) => {
-          let data = [];
-          stream.on('data', (chunk) => {
-            data.push(chunk)
-          })
-          stream.on('end', () => {
-            const image = `${Buffer.concat(data).toString('base64')}`
-            const uploadImg = {
-              url: "https://api.imgur.com/3/image",
-              headers: {
-                'Authorization': `Client-ID ${imgrConfig.imgurClientId}`,
-              },
-              form: {
-                'image': image
-              }
-            };
-            request.post(uploadImg, (err, httpResponse, body) => {
-              if (httpResponse.statusCode === 200) {
-                const resBody = JSON.parse(body);
-                const picLink = { type: 'text', text: resBody.data.link };
-                return client.replyMessage(event.replyToken, picLink);
-              } else {
-                const uploadFail = { type: 'text', text: 'Send to Imgur Fail' };
-                return client.replyMessage(event.replyToken, uploadFail);
-              }
-            })
-          })
-        })
-    }
-  }
-  return Promise.resolve(null);
-}
-*/
-
 // 新增 place-search-bot
 let conversation = {};
 function handleEvent(event) {
